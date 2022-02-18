c4c56b5d25b24806231b969d7de42a261ea1cb69
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-02-18 10:22:23 +0100

    Ej2

---
+++ UD22/db/MVC_EJ2.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS MVC_EJ2;
+USE MVC_EJ2;
+
+CREATE TABLE cliente (
+id int(11) NOT NULL AUTO_INCREMENT,
+nombre varchar(250) DEFAULT NULL,
+apellido varchar(250) DEFAULT NULL,
+direccion varchar(250) DEFAULT NULL,
+dni int(11) DEFAULT NULL,
+fecha date DEFAULT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE videos (
+id int(11) NOT NULL AUTO_INCREMENT,
+title varchar(250) DEFAULT NULL,
+director varchar(250) DEFAULT NULL,
+cli_id int(11) DEFAULT NULL,
+PRIMARY KEY (id),
+CONSTRAINT videos_fk FOREIGN KEY (cli_id) REFERENCES cliente (id)
+);
---
+++ UD22/src/main/java/modelo/Conexion.java
@@ -0,0 +1,33 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+	static String con = "jdbc:mysql://192.168.1.18:3306?useTimezone=true&serverTimezone=UTC";
+	static String user = "remote";
+	static String pass = "eE12345678";
+	public static Connection conexion;
+	//Metodo para conectarnos a la base de datos
+	public void enableConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection(con,user,pass);
+			System.out.println("Conexion establecida");
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println("No se puede conectar a la BD");
+			System.out.println(e);
+		}
+	}
+	//Metodo para cerrar la sesion de la base de datos
+	public void closeConnection() {
+		try {
+			conexion.close();
+			System.out.println("Servidor desconectado");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.out.println("Error al cerrar conexion");
+		}
+	}
+}
