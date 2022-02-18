214f9e983e6aa6ca6768840b69b63b62f5ea701b
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-02-18 12:51:04 +0100

    Ej2

--- UD22/src/main/java/UD22/UD22/App.java
+++ UD22/src/main/java/UD22/UD22/App.java
@@ -2,16 +2,34 @@ package UD22.UD22;
 
 
 import modelo.conexion.Conexion;
+import vista.vistaGeneral;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	Conexion BD = new Conexion();
-    	BD.enableConnection();
+public class App {
+	vistaGeneral ventanaGeneral;
+    public static void main( String[] args ) {
+    	App run = new App();
+    	run.iniciar();
     }
+    
+    private void iniciar() {
+		/*Se instancian las clases*/
+    	ventanaGeneral=new vistaGeneral();
+
+		
+		/*Se establecen las relaciones entre clases*/
+    	//ventanaGeneral.setCoordinador(personaController);
+
+		
+		/*Se establecen relaciones con la clase coordinador*/
+
+				
+    	ventanaGeneral.setVisible(true);
+	}
+
 }
+
+
